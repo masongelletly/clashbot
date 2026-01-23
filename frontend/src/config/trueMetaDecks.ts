8a54e012418eb24f6593dbe4b38e2e0bdbe1775f
Mason Gelletly <masongelletly@gmail.com>
2026-01-22 23:36:08 -0500

    checkpoint

---
+++ frontend/src/config/trueMetaDecks.ts
@@ -0,0 +1,86 @@
+import type { CuratedDeckDefinition } from "../utils/deckBuilder";
+
+export const trueMetaDecks: CuratedDeckDefinition[] = [
+
+  {
+    title: "Hog 2.6", 
+    cards:
+      [
+        "musketeer", "cannon", "knight", "ice golem",
+        "fireball", "ice spirit", "the log", "skeletons"
+      ],
+  },
+
+  {
+    title: "Modern Graveyard", 
+    cards:
+      [
+        "witch", "giant snowball", "mini p.e.k.k.a", "giant",
+        "graveyard", "bowler", "minions", "arrows"
+      ],
+  },
+
+  {
+    title: "X-Bow 3.0", 
+    cards:
+      [
+        "archers", "tesla", "x-bow", "knight",
+        "fireball", "skeletons", "electro spirit", "the log"
+      ],
+  },
+
+  {
+    title: "Miner Balloon", 
+    cards:
+      [
+        "musketeer", "skeletons", "ice golem", "balloon",
+        "miner", "barbarian barrel", "giant snowball", "bomb tower"
+      ],
+  },
+
+  {
+    title: "Royal Giant Fisherman", 
+    cards:
+      [
+        "royal giant", "royal ghost", "hunter", "fisherman",
+        "fireball", "skeletons", "electro spirit", "barbarian barrel"
+      ],
+  },
+
+  {
+    title: "Control Mortar", 
+    cards:
+      [
+        "skeleton barrel", "mortar", "fireball", "knight",
+        "dart goblin", "skeletons", "minions", "barbarian barrel"
+      ],
+  },
+
+  {
+    title: "Royal Hogs EQ", 
+    cards:
+      [
+        "royal hogs", "cannon", "archer queen", "earthquake",
+        "skeletons", "ice spirit", "royal delivery", "the log"
+      ],
+  },
+
+  {
+    title: "Pekka Bandit", 
+    cards:
+      [
+        "battle ram", "royal ghost", "p.e.k.k.a", "magic archer",
+        "fireball", "electro wizard", "bandit", "zap"
+      ],
+  },
+
+  {
+    title: "Clashbot Special", 
+    cards:
+      [
+        "mortar", "battle ram", "knight", "ice spirit",
+        "arrows", "baby dragon", "minions", "three musketeers"
+      ],
+  },
+
+];
